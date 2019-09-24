4d5518791f5945e62ed06a49d0780f412ff55844
liuqiyu <1064839051@qq.com>
2019-09-24 20:20:55 +0800

    feat: 部署

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -77,7 +77,8 @@ module.exports = {
                         ['/vue-admin-pro/advanced/git-hooks', 'Git Hooks'],
                         ['/vue-admin-pro/advanced/auth', '权限控制'],
                         ['/vue-admin-pro/advanced/object-freeze', '性能优化（一）：大型列表数据'],
-                        ['/vue-admin-pro/advanced/mode-and-env', '环境变量和模式']
+                        ['/vue-admin-pro/advanced/mode-and-env', '环境变量和模式'],
+                        ['/vue-admin-pro/advanced/deployment', '部署'],
                     ]
                 },
                 {
